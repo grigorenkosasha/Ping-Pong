9efffb3ebc9ebd85d2435bb8de36aa7f51ba2252
Grigorenko Alexander <mryong2103@mail.ru>
2023-12-20 20:26:57 +0300

    добавил вторую ракетку и дал им движения по кнопкам

--- pingpong.py
+++ pingpong.py
@@ -12,16 +12,21 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image,(self.rect.x, self.rect.y)) 
 
 class Player(GameSprite):
+    Key_up = K_w
+    Key_down = K_s
     def update(self):
-        global num_fire
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[self.Key_up] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
+        if keys[self.Key_down] and self.rect.y < win_height - 145:
             self.rect.y += self.speed
 
-win_width = 1050
-win_height = 1000
+class Player2(Player):
+    Key_up = K_UP
+    Key_down = K_DOWN
+
+win_width = 700
+win_height = 500
 
 window = display.set_mode((win_width, win_height))
 display.set_caption('Ping-Pong')
@@ -34,7 +39,8 @@ clock.tick(fps)
 
 game = True
 finish = False
-pl1 = Player('racket.png',20,200,5)
+pl1 = Player('racket.png',20,150,5)
+pl2 = Player2('racket.png',650,150,5)
 
 while game:
     for e in event.get():
@@ -44,6 +50,8 @@ while game:
         window.blit(background, (0,0))
         pl1.update()
         pl1.drop()
+        pl2.update()
+        pl2.drop()
         display.update()
     else:
         finish = False
