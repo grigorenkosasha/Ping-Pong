1f7a3d8774989f22992a2397d05ef613b3f3ca1f
Grigorenko Alexander <mryong2103@mail.ru>
2023-12-20 19:37:48 +0300

    Окно запускается

--- pingpong.py
+++ pingpong.py
@@ -1,1 +1,23 @@
-my ping pong game
+from pygame import *
+
+win_width = 700
+win_height = 500
+
+window = display.set_mode((win_width, win_height))
+display.set_caption('Ping-Pong')
+
+clock = time.Clock()
+fps = 60
+clock.tick(fps)
+
+game = True
+finish = False
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        display.update()
+    else:
+        finish = False
